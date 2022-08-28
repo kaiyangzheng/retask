9556b216924a6023ca7ceb59ff0ce5848baa2b24
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-08-27 22:14:25 -0700

    added sidebar active display

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -11,7 +11,8 @@ import UserInfoIcon from '@rsuite/icons/UserInfo';
 import ExitIcon from '@rsuite/icons/Exit';
 import {
     useNavigate,
-    Link
+    Link,
+    useLocation,
 } from 'react-router-dom';
 import './Sidebar.css';
 
@@ -26,7 +27,9 @@ function getWindowDimensions() {
 export default function Sidebar({ children, navColor, loginInfo, setLoginInfo }){
     const [expanded, setExpanded] = useState(false);
     const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    const [active, setActive] = useState('home');
     const navigate = useNavigate();
+    const location = useLocation();
     const handleLogout = () => {
         localStorage.removeItem('isLoggedIn');
         localStorage.removeItem('username');
@@ -65,63 +68,63 @@ export default function Sidebar({ children, navColor, loginInfo, setLoginInfo })
                                 color: navColor,    
                                 }} /> */}
                     <Sidenav.Body>
-                        <Nav>
+                        <Nav activeKey={active}>
                             <Nav.Item eventKey="home" as={Link} to="/home" icon={<HomeIcon style={{
-                                color: navColor,    
+                                color: active!='home' && navColor,    
                                 }}/>} style={{
                                     color: navColor,    
-                                    }}>
+                                    }} onClick={()=>setActive('home')}>
                                 Home
                             </Nav.Item>
                             <Nav.Item eventKey="dashboard" as={Link} to="/dashboard" icon={<DashboardIcon style={{
-                                color: navColor,    
+                                color: active!='dashboard' && navColor,    
                                 }}/>} style={{
                                     color: navColor,    
-                                    }}>
+                                    }} onClick={()=>setActive('dashboard')}>
                                 Dashboard
                             </Nav.Item>
                             <Nav.Item eventKey="calendar" as={Link} to="/calendar" icon={<CalendarIcon style={{
-                                color: navColor,    
+                                color: active!='calendar' && navColor,    
                                 }}/>} style={{
                                     color: navColor,    
-                                    }}>    
+                                    }} onClick={()=>setActive('calendar')}>    
                                 Calendar 
                             </Nav.Item>
                             <hr />
                             <Nav.Item eventKey="friends" as={Link} to="/friends" icon={<PeoplesIcon style={{
-                                color: navColor,    
+                                color: active!='friends' && navColor,    
                                 }}/>} style={{
                                     color: navColor,    
-                                    }}>
+                                    }} onClick={()=>setActive('friends')}>
                                 Friends 
                             </Nav.Item>
                             <Nav.Item eventKey="chat" as={Link} to="/chat" icon={<MessageIcon style={{
-                                color: navColor,    
+                                color: active!='chat' && navColor,    
                                 }}/>} style={{
                                     color: navColor,    
-                                    }}>
+                                    }} onClick={()=>setActive('chat')}>
                                 Chat 
                             </Nav.Item>
                             <Nav.Item eventKey="collaborations" as={Link} to="/collaborations" icon={<BranchIcon style={{
-                                color: navColor,    
+                                color: active!='collaborations' && navColor,    
                                 }}/>} style={{
                                     color: navColor,    
-                                    }}>
+                                    }} onClick={()=>setActive('collaborations')}>
                                 Collaborations
                             </Nav.Item>
                             <hr />
                             <Nav.Item eventKey="settings" as={Link} to="/settings" icon={<GearIcon style={{
-                                color: navColor,    
+                                color: active!='settings' && navColor,    
                                 }}/>} style={{
                                     color: navColor,    
-                                    }}>
+                                    }} onClick={()=>setActive('settings')}>
                                 Settings
                             </Nav.Item>
                             <Nav.Item eventKey="profile" as={Link} to="/profile" icon={<UserInfoIcon style={{
-                                color: navColor,    
+                                color: active!='profile' && navColor,    
                                 }}/>} style={{
                                     color: navColor,    
-                                    }}>
+                                    }} onClick={()=>setActive('profile')}>
                                 Account
                             </Nav.Item>
                             <Nav.Item eventKey="logout" icon={<ExitIcon style={{
