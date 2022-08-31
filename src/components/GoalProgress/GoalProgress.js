162e7eb80f6975e72abe82fdee6a774ec63bdcce
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-08-31 04:26:04 -0700

    fixed dashboard from crashing

--- src/components/GoalProgress/GoalProgress.js
+++ src/components/GoalProgress/GoalProgress.js
@@ -6,6 +6,18 @@ import {
 import './GoalProgress.css';
 
 export default function GoalProgress({taskStats, goals, dataLoaded}) {
+  let totalTasksAdded = 0;
+  let averageQuality = 0;
+  let averageRepetitions = 0;
+  let averageTimeSpent = 0;
+
+  console.log(taskStats);
+  if(!taskStats.message){
+    totalTasksAdded = taskStats.basic_info.total_tasks_added;
+    averageQuality = taskStats.stats.average_quality;
+    averageRepetitions = taskStats.stats.average_repetitions;
+    averageTimeSpent = taskStats.stats.average_time_spent;
+  }
   return <>
   <Panel className="goal-process" bordered>
     <h2>Task report</h2>
@@ -13,13 +25,13 @@ export default function GoalProgress({taskStats, goals, dataLoaded}) {
         <Panel className="goal-process-item">
             {dataLoaded ? <div className="goal-process-content">
                 <div className="goal-process-stats">
-                    <span className="user-value">{taskStats.basic_info.total_tasks_added}</span>
+                    <span className="user-value">{totalTasksAdded}</span>
                     <span className="left-slash">/</span>
                     <span className="goal-value">{goals.total_added}</span>
                     <span className="description">&nbsp;goal</span>
                 </div>
                 <div className="goal-process-percentage">
-                    ({Math.round((taskStats.basic_info.total_tasks_added/goals.total_added)*100*10)/10}%)
+                    ({Math.round((totalTasksAdded/goals.total_added)*100*10)/10}%)
                 </div>
                 <div className="goal-name">
                     Tasks added
@@ -29,13 +41,13 @@ export default function GoalProgress({taskStats, goals, dataLoaded}) {
         <Panel className="goal-process-item border-top-768 mt-20-768">
             {dataLoaded ? <div className="goal-process-content">
                 <div className="goal-process-stats">
-                    <span className="user-value">{Math.round(taskStats.stats.average_quality*10)/10}</span>
+                    <span className="user-value">{Math.round(averageQuality*10)/10}</span>
                     <span className="left-slash">/</span>
                     <span className="goal-value">{goals.average_quality}</span>
                     <span className="description">&nbsp;goal</span>
                 </div>
                 <div className="goal-process-percentage">
-                    ({Math.round((taskStats.stats.average_quality/goals.average_quality)*100*10)/10}%)
+                    ({Math.round((averageQuality/goals.average_quality)*100*10)/10}%)
                 </div>
                 <div className="goal-name">
                     Average quality
@@ -47,13 +59,13 @@ export default function GoalProgress({taskStats, goals, dataLoaded}) {
         <Panel className="goal-process-item border-top">
             {dataLoaded ? <div className="goal-process-content">
                 <div className="goal-process-stats">
-                    <span className="user-value">{Math.round(taskStats.stats.average_repetitions*10)/10}</span>
+                    <span className="user-value">{Math.round(averageRepetitions*10)/10}</span>
                     <span className="left-slash">/</span>
                     <span className="goal-value">{goals.average_repetitions}</span>
                     <span className="description">&nbsp;goal</span>
                 </div>
                 <div className="goal-process-percentage">
-                    ({Math.round((taskStats.stats.average_repetitions/goals.average_repetitions)*100*10)/10}%)
+                    ({Math.round((averageRepetitions/goals.average_repetitions)*100*10)/10}%)
                 </div>
                 <div className="goal-name">
                     Average repetitions
@@ -63,13 +75,13 @@ export default function GoalProgress({taskStats, goals, dataLoaded}) {
         <Panel className="goal-process-item border-top mt-20-768">
             {dataLoaded ? <div className="goal-process-content">
                 <div className="goal-process-stats">
-                    <span className="user-value">{Math.round(taskStats.stats.average_time_spent*10)/10}</span>
+                    <span className="user-value">{Math.round(averageTimeSpent*10)/10}</span>
                     <span className="left-slash">/</span>
                     <span className="goal-value">{goals.average_time_spent}</span>
                     <span className="description">s&nbsp;limit</span>
                 </div>
                 <div className="goal-processs-percentage">
-                ({Math.round((taskStats.stats.average_time_spent/goals.average_time_spent)*100*10)/10}%)
+                ({Math.round((averageTimeSpent/goals.average_time_spent)*100*10)/10}%)
                 </div>
                 <div className="goal-name">
                     Average time spent
